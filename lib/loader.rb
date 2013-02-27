70b86b78509a2f97c139a2cc3539adcf31951f21
Javier Juarez <javier.juarez@gmail.com>
2013-02-27 17:04:15 +0100

    Deleting old files

--- lib/loader.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'mini_logger'
-
-
-begin
-  Dir.glob( File.join( File.dirname( __FILE__ ), "**", "*.rb" ) ).each { |library| require library }  
-rescue LoadError => le
-  MiniLogger.error( le.message )
-end
